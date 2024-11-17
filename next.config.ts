f2c2ffc0aa33e1f65504524753658edd2f2b3d3f
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-11-17 22:56:37 +0000

    init template

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enable React Strict Mode to help identify potential problems in the application
+
+  swcMinify: true, // Enable SWC for minifying JavaScript code for faster builds and better performance
+
+  productionBrowserSourceMaps: true, // Enable source maps in production to help with debugging
 };
 
 export default nextConfig;
